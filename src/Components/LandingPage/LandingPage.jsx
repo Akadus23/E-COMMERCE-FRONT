b2c100a575fb548bfab58cfc49fca060db814f90
dalegaz <indicevertical@gmail.com>
2023-07-12 23:49:02 -0300

    mier5

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import s from "./LandingPage.module.css";
 import Carrusel from "./Carrusel/Carrusel";
 import Productos from "./Productos/Productos";
@@ -7,12 +7,28 @@ import Categorias from "./Categorias/Categorias";
 import Reseñas from "./Reseñas/Reseña";
 import Tiendas from "./Tiendas/Tienda";
 import Info from "./Info/Info";
+import { Dialog, CircularProgress, Backdrop } from '@mui/core';
+
 
 function SwipeableTextMobileStepper() {
   // const theme = useTheme();
+  const [open, setOpen] = useState(true);
+
+  useEffect(() => {
+    // Cerrar el modal después de 5 segundos
+    const timer = setTimeout(() => {
+      setOpen(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div>
+      <Backdrop open={open} style={{ zIndex: 999, color: '#fff', opacity: 0.5 }} />
+      <Dialog open={open} maxWidth="xs">
+        <CircularProgress size={80} />
+      </Dialog>
       <Carrusel></Carrusel>
       <p className={s.producto}>Productos</p>
       <Productos></Productos>
